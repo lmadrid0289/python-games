60f093e5f318b0e476683984f3e5b77a3e89d256
luis <lmadrid0289@gmail.com>
2020-09-07 16:11:06 -0400

    first commit

---
+++ Pong1.py
@@ -0,0 +1,17 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("turtle handle @luis Madrid")
+wn.bgcolor("green")
+wn.setup(width=800, height=600)
+wn.tracer(0) 
+
+#main game loop
+
+#paddle A
+#paddle B
+#ball  
+while True:
+    wn.update()
+
+  
